a46225560608ba593189a2d75e14252b0a6634c4
Adam Stępień <vesemir2k@gmail.com>
2024-08-28 18:25:11 +0200

    ✨ (HomeView.vue): Add functionality to increase and decrease a counter value on button click 📝 (HomeView.vue): Update counter title dynamically based on user input in the input field

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h3>{{ counterTitle }}</h3>
+
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterTitle" type="text" />
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(0),
+  counterTitle = ref("My Counter");
+
+const increaseCounter = () => {
+  counter.value++;
+};
+
+const decreaseCounter = () => {
+  counter.value--;
+};
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+
+.edit {
+  margin-top: 60px;
+}
+</style>
