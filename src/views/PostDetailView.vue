b4f6b6f6b63f8121747be63910bd20831cc25c12
Adam Stępień <vesemir2k@gmail.com>
2024-08-29 18:40:56 +0200

    ✨ (App.vue): Update the link in the navigation bar from "/about" to "/posts" to navigate to the Posts page instead of the About page ✨ (router/index.js): Add a new route for "/postDetail/:id" to display the details of a specific post 📝 (PostDetailView.vue): Create a new component for displaying the details of a post 📝 (PostsView.vue): Create a new component for displaying a list of posts

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="post-detail">
+    <h1>This is a post page</h1>
+  </div>
+</template>
+
+<script setup>
+// & imports
+import { vAutofocus } from "@/directives/vAutofocus";
+</script>
