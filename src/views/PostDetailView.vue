509e5366289cdae48a07f54488b9c19cde22a0a2
Adam Stępień <vesemir2k@gmail.com>
2024-08-29 18:58:45 +0200

    ✨ (PostDetailView.vue): Add functionality to navigate to home page after 3 seconds and navigate to the first post page. Add buttons for these actions to enhance user experience.

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -2,19 +2,45 @@
   <div class="post-detail">
     <h1>This is a post page</h1>
     <p>Display the content of post with ID of {{ $route.params.id }} here!</p>
+
     <div>
       <button @click="showPostId">Show Post ID</button>
     </div>
+
+    <div>
+      <button @click="goHomeIn3Seconds">Go Home in 3 seconds</button>
+    </div>
+
+    <div>
+      <button @click="goToFirstPost">Go to firs post</button>
+    </div>
+
     <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
+const router = useRouter();
 
 const showPostId = () => {
   alert(`The ID of this post is: ${route.params.id}`);
+};
+
+const goHomeIn3Seconds = () => {
+  setTimeout(() => {
+    router.push({ name: "home" });
+  }, 3000);
+};
+
+const goToFirstPost = () => {
+  router.push({
+    name: "postDetail",
+    params: {
+      id: "id1",
+    },
+  });
 };
 </script>
