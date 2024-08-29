66e7ae8cfb799c1ff228374e42badbb878857558
Adam Stępień <vesemir2k@gmail.com>
2024-08-30 00:52:48 +0200

    ✨ (App.vue): Add a new "Modals" link to the navigation bar in the app ✨ (router/index.js): Add a new route for "/modals" to display the ModalsView component ✨ (views/ModalsView.vue): Create a new ModalsView component to display modals with show/hide functionality

---
+++ src/views/ModalsView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="modals">
+    <h1>Modals</h1>
+    <button @click="showModal = true">Show modal</button>
+    <div v-if="showModal" class="modal">
+      <h1>This is a modal</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum hic
+        nesciunt, labore, pariatur blanditiis porro magnam culpa quisquam ipsum
+        neque eos numquam tempora doloribus quas possimus recusandae nulla odit!
+        Blanditiis?
+      </p>
+      <button @click="showModal = false">Hide modal</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// & imports
+import { ref } from "vue";
+
+// & modals
+const showModal = ref(false);
+</script>
+
+<style scoped>
+.modal {
+  background-color: beige;
+  color: black;
+  padding: 30px;
+}
+
+p {
+  margin-bottom: 10px;
+}
+</style>
