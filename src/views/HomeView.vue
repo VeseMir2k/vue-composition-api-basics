4963c079fb4cd287004c2378992ab253effe3bea
Adam Stępień <vesemir2k@gmail.com>
2024-08-30 00:42:17 +0200

    📝 (HomeView.vue): add a ref to the app title element to be able to access it in the script setup 🐛 (HomeView.vue): fix console log message to correctly display the width of the app title element

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h2>{{ appTitle }}</h2>
+    <h2 ref="appTitleRef">{{ appTitle }}</h2>
 
     <h3>{{ counterData.title }}</h3>
 
@@ -23,14 +23,16 @@
 
 <script setup>
 // & imports
-import { reactive, computed, watch, onMounted } from "vue";
+import { ref, reactive, computed, watch, onMounted } from "vue";
 import { vAutofocus } from "@/directives/vAutofocus";
 
 // & app title
 const appTitle = "My Amazing Counter App";
 
+const appTitleRef = ref(null);
+
 onMounted(() => {
-  console.log("Do stuff related to App Title");
+  console.log(`The app title is ${appTitleRef.value.offsetWidth}px wide!`);
 });
 
 // & counter
