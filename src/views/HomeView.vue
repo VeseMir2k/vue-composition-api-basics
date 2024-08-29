d577807729a5af9312de466f1bc4a6d0347c1d38
Adam Stępień <vesemir2k@gmail.com>
2024-08-29 18:03:58 +0200

    📝 (HomeView.vue): update imports and add comments for better code readability 📝 (HomeView.vue): add onMounted hook for app title and counter functionality for better lifecycle management

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,10 +22,17 @@
 </template>
 
 <script setup>
-import { reactive, computed, watch, onBeforeUpdate, onUpdated } from "vue";
+// & imports
+import { reactive, computed, watch, onMounted } from "vue";
 
+// & app title
 const appTitle = "My Amazing Counter App";
 
+onMounted(() => {
+  console.log("Do stuff related to App Title");
+});
+
+// & counter
 const counterData = reactive({
   count: 0,
   title: "My counter",
@@ -53,12 +60,8 @@ const decreaseCounter = (amount) => {
   counterData.count -= amount;
 };
 
-onBeforeUpdate(() => {
-  console.log("onBeforeUpdate");
-});
-
-onUpdated(() => {
-  console.log("onUpdated");
+onMounted(() => {
+  console.log("Do stuff related to Counter");
 });
 </script>
 
