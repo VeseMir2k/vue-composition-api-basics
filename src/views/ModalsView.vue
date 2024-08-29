de0611a345df99a8149f4ae8fe1c14d3e5ae0405
Adam Stępień <vesemir2k@gmail.com>
2024-08-30 00:58:10 +0200

    ✨ (index.html): Add a new div with class "modals-container" to index.html for modals support 📝 (base.css): Update font-family property in base.css for better readability and consistency 📝 (base.css): Add styles for .modals-container class in base.css 📝 (ModalsView.vue): Refactor ModalsView.vue to use teleport for better structure and readability 📝 (ModalsView.vue): Update styles for .modal class in ModalsView.vue for better positioning and visibility

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,16 +2,18 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modal</button>
-    <div v-if="showModal" class="modal">
-      <h1>This is a modal</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum hic
-        nesciunt, labore, pariatur blanditiis porro magnam culpa quisquam ipsum
-        neque eos numquam tempora doloribus quas possimus recusandae nulla odit!
-        Blanditiis?
-      </p>
-      <button @click="showModal = false">Hide modal</button>
-    </div>
+    <teleport to=".modals-container">
+      <div v-if="showModal" class="modal">
+        <h1>This is a modal</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum hic
+          nesciunt, labore, pariatur blanditiis porro magnam culpa quisquam
+          ipsum neque eos numquam tempora doloribus quas possimus recusandae
+          nulla odit! Blanditiis?
+        </p>
+        <button @click="showModal = false">Hide modal</button>
+      </div>
+    </teleport>
   </div>
 </template>
 
@@ -28,6 +30,12 @@ const showModal = ref(false);
   background-color: beige;
   color: black;
   padding: 30px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
 }
 
 p {
