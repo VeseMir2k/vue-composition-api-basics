76cb84193dddd5df92658a4cc08cff6ff53a6bb5
Adam Stępień <vesemir2k@gmail.com>
2024-08-30 17:48:16 +0200

    ✨ (Modal.vue): Add a new Modal component to display a modal dialog box in the application 📝 (router/index.js): Update routing to include new views for ModalsView and PostDetailView 🔧 (ModalsView.vue): Refactor ModalsView to use the new Modal component for displaying modals in the application

---
+++ src/components/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <teleport to=".modals-container">
+    <div class="modal">
+      <h1>This is a modal</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum hic
+        nesciunt, labore, pariatur blanditiis porro magnam culpa quisquam ipsum
+        neque eos numquam tempora doloribus quas possimus recusandae nulla odit!
+        Blanditiis?
+      </p>
+      <button>Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<style scoped>
+.modal {
+  background-color: beige;
+  color: black;
+  padding: 30px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+
+p {
+  margin-bottom: 10px;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,43 +2,27 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modal</button>
-    <teleport to=".modals-container">
-      <div v-if="showModal" class="modal">
-        <h1>This is a modal</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum hic
-          nesciunt, labore, pariatur blanditiis porro magnam culpa quisquam
-          ipsum neque eos numquam tempora doloribus quas possimus recusandae
-          nulla odit! Blanditiis?
-        </p>
-        <button @click="showModal = false">Hide modal</button>
-      </div>
-    </teleport>
+    <Modal v-if="showModal" />
   </div>
 </template>
 
 <script setup>
 // & imports
 import { ref } from "vue";
+import Modal from "@/components/Modal.vue";
 
 // & modals
 const showModal = ref(false);
 </script>
 
-<style scoped>
-.modal {
-  background-color: beige;
-  color: black;
-  padding: 30px;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
+<!--
+<script>
+import Modal from "@/components/Modal.vue";
 
-p {
-  margin-bottom: 10px;
-}
-</style>
+export default {
+  components: {
+    Modal,
+  },
+};
+</script>
+-->
